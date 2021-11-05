a3bbe855bfe5710d089a3330db09d453695901c6
Varyag-Ingvar <varyag8512@gmail.com>
2021-11-05 15:09:22 +0400

    pep8 formatted

--- traversing all digits of a number using a while loop.py
+++ traversing all digits of a number using a while loop.py
@@ -1,4 +1,3 @@
-
 # обход всех цифр числа с помощью цикла while
 # traversing all digits of a number using a while loop
 
@@ -13,15 +12,19 @@ minim = 9
 while x > 0:
     last = x % 10
     count = count + 1
+
     if last % 2 == 0:
         count_even += 1
     summ = summ + last
     pr = pr * last
+
     if last > maxim:
         maxim = last
+
     if last < minim:
         minim = last
-    x = x//10
+
+    x = x // 10
 
 print(f'Total digits count in the number: {count}')
 print(f'Total even digits count in the number: {count_even}')
@@ -29,4 +32,3 @@ print(f'The sum of all the digits in the number: {summ}')
 print(f'The product of all the digits in the number: {pr}')
 print(f'Maximum digit in the number: {maxim}')
 print(f'Minimum digit in the number: {minim}')
-
